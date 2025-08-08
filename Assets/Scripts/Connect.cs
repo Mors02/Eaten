1f2685699994ea0e0fd584409aae128591a7990b
Leonardo Morselli <leonardo.morselli02@gmail.com>
2025-08-08 21:07:57 +0200

    feat(main): non funzia nu cazzoooo

--- Assets/Scripts/Connect.cs
+++ Assets/Scripts/Connect.cs
@@ -6,20 +6,38 @@ public class Connect : MonoBehaviour, IPointerEnterHandler, IPointerUpHandler, I
     private bool _clicking = false;
     [SerializeField]
     private GameObject _line;
-    private GameObject _canvas;
+    private Canvas _canvas;
+    private RectTransform _lineTransform;
+    private RectTransform _dotRectTransform;
+    private Camera _camera;
+
+    private void Start()
+    {
+        _canvas = GetComponentInParent<Canvas>();
+        _dotRectTransform = GetComponent<RectTransform>();
+        _camera = _canvas.worldCamera ?? Camera.main;
+    }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("Clicked on " + gameObject.name);
         _clicking = true;
-        GameObject line = GameObject.Instantiate(_line, this.transform.position, Quaternion.identity, this.transform);
-        RectTransform t = line.GetComponent<RectTransform>();
-        line.transform.position = new Vector3(this.transform.position.x + (t.sizeDelta.x/2), this.transform.position.y - (t.sizeDelta.y/2));
+
+        // Instantiate line as child of canvas (not this dot)
+        GameObject line = Instantiate(_line, _canvas.transform);
+        _lineTransform = line.GetComponent<RectTransform>();
+
+        // Set initial position and size
+        _lineTransform.anchoredPosition = _dotRectTransform.anchoredPosition;
+        _lineTransform.sizeDelta = new Vector2(0, _lineTransform.sizeDelta.y);
+
+        // Set pivot to left (0, 0.5) so line extends from the dot
+        _lineTransform.pivot = new Vector2(0, 0.5f);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        
+        // Handle dot connection logic here if needed
     }
 
     public void OnPointerUp(PointerEventData eventData)
@@ -28,15 +46,33 @@ public class Connect : MonoBehaviour, IPointerEnterHandler, IPointerUpHandler, I
         _clicking = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //if (_clicking)
-            //Debug.Log("Clicking");
-    }
+        if (_clicking && _lineTransform != null)
+        {
+            // Convert mouse position to canvas space
+            Vector2 mouseCanvasPos;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                _canvas.GetComponent<RectTransform>(),
+                Input.mousePosition,
+                _camera,
+                out mouseCanvasPos
+            );
 
-    public void Start()
-    {
-        this._canvas = GameObject.FindGameObjectWithTag("Canvas");
+            // Calculate direction and distance
+            Vector2 dotPosition = _dotRectTransform.anchoredPosition;
+            Vector2 direction = mouseCanvasPos - dotPosition;
+            float distance = direction.magnitude;
+
+            // Update line length
+            _lineTransform.sizeDelta = new Vector2(distance, _lineTransform.sizeDelta.y);
+
+            // Calculate and apply rotation
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            _lineTransform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            // Keep line positioned at the dot
+            _lineTransform.anchoredPosition = dotPosition;
+        }
     }
 }
