560a48ba4539e96aa53bb827093285447b745d91
Leonardo Morselli <leonardo.morselli02@gmail.com>
2025-08-13 17:48:02 +0200

    feat(grid): the single block is working

--- Assets/Scripts/Connect.cs
+++ Assets/Scripts/Connect.cs
@@ -3,76 +3,32 @@ using UnityEngine.EventSystems;
 
 public class Connect : MonoBehaviour, IPointerEnterHandler, IPointerUpHandler, IPointerDownHandler
 {
-    private bool _clicking = false;
     [SerializeField]
-    private GameObject _line;
-    private Canvas _canvas;
-    private RectTransform _lineTransform;
-    private RectTransform _dotRectTransform;
-    private Camera _camera;
+    private GameObject _line; 
+    private ConnectManager _cm;
 
     private void Start()
     {
-        _canvas = GetComponentInParent<Canvas>();
-        _dotRectTransform = GetComponent<RectTransform>();
-        _camera = _canvas.worldCamera ?? Camera.main;
+        _cm = GameObject.FindGameObjectWithTag("Canvas").GetComponent<ConnectManager>();
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("Clicked on " + gameObject.name);
-        _clicking = true;
-
-        // Instantiate line as child of canvas (not this dot)
-        GameObject line = Instantiate(_line, _canvas.transform);
-        _lineTransform = line.GetComponent<RectTransform>();
-
-        // Set initial position and size
-        _lineTransform.anchoredPosition = _dotRectTransform.anchoredPosition;
-        _lineTransform.sizeDelta = new Vector2(0, _lineTransform.sizeDelta.y);
-
-        // Set pivot to left (0, 0.5) so line extends from the dot
-        _lineTransform.pivot = new Vector2(0, 0.5f);
+        _cm.SelectStartingBlock(this.transform);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        // Handle dot connection logic here if needed
+        if (_cm.IsClicking() && !_cm.AlreadySelected(gameObject.name))
+        {
+            Debug.Log("Can add " + gameObject.name);
+        }
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        Debug.Log("Stopped clicking");
-        _clicking = false;
+        this._cm.StopClicking();
     }
 
-    void Update()
-    {
-        if (_clicking && _lineTransform != null)
-        {
-            // Convert mouse position to canvas space
-            Vector2 mouseCanvasPos;
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(
-                _canvas.GetComponent<RectTransform>(),
-                Input.mousePosition,
-                _camera,
-                out mouseCanvasPos
-            );
-
-            // Calculate direction and distance
-            Vector2 dotPosition = _dotRectTransform.anchoredPosition;
-            Vector2 direction = mouseCanvasPos - dotPosition;
-            float distance = direction.magnitude;
-
-            // Update line length
-            _lineTransform.sizeDelta = new Vector2(distance, _lineTransform.sizeDelta.y);
-
-            // Calculate and apply rotation
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            _lineTransform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
-            // Keep line positioned at the dot
-            _lineTransform.anchoredPosition = dotPosition;
-        }
-    }
+    
 }
---
+++ Assets/Scripts/ConnectManager.cs
@@ -0,0 +1,115 @@
+using UnityEngine;
+using System;
+
+public class ConnectManager : MonoBehaviour
+{
+    private bool _clicking = false;
+    private RectTransform _lineTransform, _currectRectTransform;
+    private Canvas _canvas;
+    [SerializeField]
+    private GameObject _line;
+    private Vector2 _defaultPosition, _dotStartPosition;
+    private int _currentBlock = -1;
+    private Vector2 _offset;
+    private int[] _selectedBlocks;
+    private int _howManyBlocks = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _canvas = GetComponentInParent<Canvas>();
+        _selectedBlocks = new int[4];
+    }
+
+    public void SelectStartingBlock(Transform transform)
+    {
+        Debug.Log("Clicked on " + gameObject.name);
+        _clicking = true;
+
+        // Instantiate line as child of canvas (not this dot)
+        GameObject line = Instantiate(_line, transform.position, Quaternion.identity, transform);
+        _lineTransform = line.GetComponent<RectTransform>();
+
+        // Set initial position and size
+        //_lineTransform.anchoredPosition = _dotRectTransform.anchoredPosition;
+        //_lineTransform.sizeDelta = new Vector2(0, _lineTransform.sizeDelta.y);
+        _currectRectTransform = transform.gameObject.GetComponent<RectTransform>();
+
+        // Store the dot position in canvas coordinates for consistent calculations
+        _dotStartPosition = _currectRectTransform.position;//Input.mousePosition / _canvas.scaleFactor;
+
+        _defaultPosition = new Vector3(this.transform.position.x + (_lineTransform.sizeDelta.x / 2), transform.position.y);
+        line.transform.position = this._defaultPosition;
+        //Vector2 mousePos = (Input.mousePosition / _canvas.scaleFactor);
+        //_offset = _dotStartPosition - _currectRectTransform;
+        //Debug.Log(_offset);
+        // Set pivot to left (0, 0.5) so line extends from the dot
+        _lineTransform.pivot = new Vector2(0, 0.5f);
+
+        _currentBlock = Int32.Parse(transform.gameObject.name);
+        _selectedBlocks[0] = _currentBlock;
+        Debug.Log(_currentBlock);
+    }
+
+    public void StopClicking()
+    {
+        _clicking = false;
+        Destroy(this._lineTransform.gameObject);
+        _currectRectTransform = null;
+        _currentBlock = -1;
+        _selectedBlocks = new int[4];
+        _howManyBlocks = 0;
+    }
+    
+    public bool IsClicking()
+    {
+        return _clicking;
+    }
+
+    public bool AlreadySelected(string name)
+    {
+        int id = Int32.Parse(name);
+        for (int i = 0; i<_howManyBlocks; i++)
+        {
+            if (_selectedBlocks[i] == id)
+                return true;
+        }
+        return false;
+    }
+
+
+    void Update()
+    {
+        if (_clicking && _lineTransform != null)
+        {
+            // Convert mouse position to canvas space
+            Vector2 mouseCanvasPos;
+            /*RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                _canvas.GetComponent<RectTransform>(),
+                Input.mousePosition,
+                _camera,
+                out mouseCanvasPos
+            );*/
+            mouseCanvasPos = Input.mousePosition / _canvas.scaleFactor;
+
+            //mouseCanvasPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+            
+            // Calculate direction and distance
+            Vector2 dotPosition = _currectRectTransform.position;
+            Vector2 direction = (mouseCanvasPos + _offset) - _dotStartPosition;
+            float distance = direction.magnitude;
+
+            // Update line length
+            _lineTransform.sizeDelta = new Vector2(distance, _lineTransform.sizeDelta.y);
+
+            // Calculate and apply rotation
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            _lineTransform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            // Keep line positioned at the dot
+            _lineTransform.position = dotPosition;
+        }
+    }
+
+
+}
